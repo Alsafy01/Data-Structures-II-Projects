5eff3571418f09925f8423fa1dce4fb1f55705b8
Alsafy01 <alsafymedo22@gmail.com>
2023-04-28 17:26:00 +0300

    final version

--- Benchmark/main.py
+++ Benchmark/main.py
@@ -9,24 +9,26 @@ def detect_image(image):
     while True:
         try:
             location = pyautogui.locateOnScreen(image)
-            return
+            if(location != None):
+                print(location)
+                return
         except Exception as e:
             pass
 
 
-def bechmark(app_name):
+def benchmark(app_name):
     a = []
     match app_name:
         case "whatsapp":
-            image = "img.png"
-        case "teams":
-            image = "img.png"
-        case "chrome":
-            image = "img.png"
-        case "edge":
-            image = "img.png"
-        case "netbeans":
-            image = "img.png"
+            image = "whatsapp.png"
+        case "onenote":
+            image = "onenote.png"
+        case "discord":
+            image = "discord.png"
+        case "excel":
+            image = "excel.png"
+        case "codeblocks":
+            image = "codeblocks.png"
     for i in range(0, 5):
         open(app_name)
         start = time.time()
@@ -38,6 +40,10 @@ def bechmark(app_name):
     return a
 
 
-a = [bechmark("whatsapp"), bechmark("whatsapp"), bechmark("whatsapp"), bechmark("whatsapp"), bechmark("whatsapp")]
-for i in a:
-    print(i)
+apps = ["whatsapp", "codeblocks", "excel", "onenote", "discord"]
+a = []
+for j in apps:
+    a.append(benchmark(j))
+
+for i in range(0, 5):
+    print(f"{apps[i]} time = {a[i]}")
