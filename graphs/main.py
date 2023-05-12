c99833c6c62d9c4cfc3d3a102cf171858db3cc4e
Alsafy01 <alsafymedo22@gmail.com>
2023-05-13 00:26:19 +0300

    Prim's

--- graphs/main.py
+++ graphs/main.py
@@ -1,6 +1,85 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from typing import List, Dict  # For annotations
 
+
+class Node:
+
+    def __init__(self, arg_id):
+        self._id = arg_id
+
+
+class Graph:
+
+    def __init__(self, source: int, adj_list: Dict[int, List[int]]):
+        self.source = source
+        self.adjlist = adj_list
+
+    def PrimsMST(self) -> int:
+
+        # Priority queue is implemented as a dictionary with
+        # key as an object of 'Node' class and value as the cost of
+        # reaching the node from the source.
+        # Since the priority queue can have multiple entries for the
+        # same adjacent node but a different cost, we have to use objects as
+        # keys so that they can be stored in a dictionary.
+        # [As dictionary can't have duplicate keys so objectify the key]
+
+        # The distance of source node from itself is 0. Add source node as the first node
+        # in the priority queue
+        priority_queue = {Node(self.source): 0}
+        added = [False] * len(self.adjlist)
+        min_span_tree_cost = 0
+
+        while priority_queue:
+            # Choose the adjacent node with the least edge cost
+            node = min(priority_queue, key=priority_queue.get)  # min priority queue
+            cost = priority_queue[node]
+
+            # Remove the node from the priority queue
+            del priority_queue[node]
+
+            if added[node._id] == False:
+                min_span_tree_cost += cost
+                added[node._id] = True
+                print("Added Node : " + str(node._id) + ", cost now : " + str(min_span_tree_cost))
+
+                for item in self.adjlist[node._id]:
+                    adjnode = item[0]
+                    adjcost = item[1]
+                    if added[adjnode] == False:
+                        priority_queue[Node(adjnode)] = adjcost
+
+        return min_span_tree_cost
+
+
+def matrix_graph_plug(matrix):
+    vertex_num = len(matrix)
+    if (vertex_num != len(matrix[0])):
+        raise Exception("Sorry, matrix have to be square matrix")
+    g1_edges_from_node = {}
+    # Outgoing edges from the node: (adjacent_node, cost) in graph 1.
+    for i in range(0, vertex_num):
+        count = 0
+        a = []
+        for weight in matrix[i]:
+            if (weight != 0):
+                a.append((count, weight))
+                print(a)
+            count += 1
+        g1_edges_from_node[i] = a
+
+    # g1_edges_from_node[0] = [(1, 1), (2, 2), (3, 1), (4, 1), (5, 2), (6, 1)]
+    # g1_edges_from_node[1] = [(0, 1), (2, 2), (6, 2)]
+    # g1_edges_from_node[2] = [(0, 2), (1, 2), (3, 1)]
+    # g1_edges_from_node[3] = [(0, 1), (2, 1), (4, 2)]
+    # g1_edges_from_node[4] = [(0, 1), (3, 2), (5, 2)]
+    # g1_edges_from_node[5] = [(0, 2), (4, 2), (6, 1)]
+    # g1_edges_from_node[6] = [(0, 1), (2, 2), (5, 1)]
+
+    g1 = Graph(0, g1_edges_from_node)
+    cost = g1.PrimsMST()
+    print("Cost of the minimum spanning tree in graph 1 : " + str(cost) + "\n")
 
 def print_graph(matrix):
     vertex_num = len(matrix)
@@ -31,9 +110,12 @@ def print_graph(matrix):
         plt.show()
 
 
-G = [[0, 19, 5, 0, 0],
-     [19, 0, 5, 9, 2],
-     [5, 5, 0, 1, 6],
-     [0, 9, 1, 0, 1],
-     [0, 2, 6, 1, 0]]
+G = [[0, 1, 2, 1, 1, 2, 1],
+     [1, 0, 2, 0, 0, 0, 2],
+     [2, 2, 0, 1, 0, 0, 0],
+     [1, 0, 1, 0, 2, 0, 0],
+     [1, 0, 0, 2, 0, 2, 0],
+     [2, 0, 0, 0, 2, 0, 1],
+     [1, 0, 2, 0, 0, 1, 0]]
+matrix_graph_plug(G)
 print_graph(G)
